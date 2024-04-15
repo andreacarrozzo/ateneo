119241c68032f52097e1d7522238b4bbe4d2437c
andre <andre@AndreaPC.station>
2024-04-15 17:36:53 +0200

    dati database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO studente VALUES('1', '12345', 'RSSMRI0123456789', 'Mario', 'Rossi', 'M', '2000-05-30');
+INSERT INTO studente VALUES('2', '23456', 'BNCNDR0123456789', 'Andrea', 'Bianchi', 'M', '2003-07-07');
+INSERT INTO studente VALUES('3', '34567', 'VRDCRL0123456789', 'Carlo', 'Verdi', 'M', '1997-01-07');
+
+INSERT INTO materia VALUES('1', 'MAT01', 'Analisi I', '9');
+INSERT INTO materia VALUES('2', 'MAT02', 'Fisica', '9');
+INSERT INTO materia VALUES('3', 'MAT03', 'Database', '12');
+
+INSERT INTO esame VALUES('1', '1', '1', '2023-05-07', '30', TRUE);
+INSERT INTO esame VALUES('2', '1', '2', '2023-11-09', '27', FALSE);
+INSERT INTO esame VALUES('3', '2', '1', '2023-06-13', '24', FALSE);
+INSERT INTO esame VALUES('4', '2', '2', '2024-01-08', '28', FALSE);
+INSERT INTO esame VALUES('5', '2', '3', '2024-02-18', '30', TRUE);
