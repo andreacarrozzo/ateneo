df8544574416c5d0ec44c5e4a8c67954df625a43
giusa <giusa@salbix.homenet.telecomitalia.it>
2024-04-15 17:35:22 +0200

    aggiunta parte java in studenti.jsp

--- src/main/java/org/generationitaly/ateneo/controller/StudentiServlet.java
+++ src/main/java/org/generationitaly/ateneo/controller/StudentiServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.generationitaly.ateneo.entity.Studente;
@@ -18,7 +19,19 @@ public class StudentiServlet extends HttpServlet {
 	private static StudenteRepository studenteRepository = new StudenteRepositoryImpl();
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		List<Studente> studenti = studenteRepository.findAll();
+		//List<Studente> studenti = studenteRepository.findAll();
+		Studente studente = new Studente();
+		studente.setMatricola("123");
+		studente.setNome("Mario");
+		studente.setCognome("rossi");
+		Studente studente2 = new Studente();
+		studente2.setMatricola("234");
+		studente2.setNome("Francesca");
+		studente2.setCognome("Verdi");
+		List<Studente> studenti = new ArrayList<Studente>();
+		studenti.add(studente);
+		studenti.add(studente2);
+		
 		request.setAttribute("studenti", studenti);
 		request.getRequestDispatcher("studenti.jsp").forward(request, response);
 	}
--- src/main/webapp/studenti.jsp
+++ src/main/webapp/studenti.jsp
@@ -1,3 +1,5 @@
+<%@page import="java.util.List"%>
+<%@page import="org.generationitaly.ateneo.entity.Studente"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
 	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
@@ -7,6 +9,7 @@
 <title>Insert title here</title>
 </head>
 <body>
+	<% List<Studente> studenti = (List<Studente>) request.getAttribute("studenti"); %>
 	<table>
 		<thead>
 			<tr>
@@ -16,11 +19,13 @@
 			</tr>
 		</thead>
 		<tbody>
+		<% for(Studente studente : studenti){ %>
 			<tr>
-				<td>23</td>
-				<td>Carlo</td>
-				<td>Rossi</td>
+				<td><%=studente.getMatricola() %></td>
+				<td><%=studente.getNome() %></td>
+				<td><%=studente.getCognome() %></td>
 			</tr>
+			<%} %>
 		</tbody>
 	</table>
 </body>
