57ddcd5759f5a48325df276e41cb6a6cef313d5e
andre <andre@AndreaPC.station>
2024-04-15 14:29:10 +0200

    caricato studente

--- src/main/java/org/generationitaly/ateneo/entity/Studente.java
+++ src/main/java/org/generationitaly/ateneo/entity/Studente.java
@@ -1,5 +1,104 @@
 package org.generationitaly.ateneo.entity;
 
-public class Studente {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+/*
+id INT NOT NULL AUTO_INCREMENT,
+matricola CHAR(5) NOT NULL,
+codice_fiscale CHAR(16) NOT NULL,
+nome VARCHAR(45) NOT NULL,
+cognome VARCHAR(45) NOT NULL,
+sesso CHAR(1) NOT NULL,
+data_nascita DATE NOT NULL,
+PRIMARY KEY(id),
+UNIQUE(matricola),
+UNIQUE(codice_fiscale),
+CHECK(sesso IN('M', 'F'))
+*/
+
+@Entity 
+@Table(name = "studente")
+public class Studente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	private int id;
+	
+	@Column(name = "matricola", nullable = false, length = 6)
+	private String matricola;
+	
+	@Column(name = "nome", nullable = false, length = 45)
+	private String nome;
+	
+	@Column(name = "cognome", nullable = false, length = 45)
+	private String cognome;
+	
+	@Column(name = "sesso", nullable = false, length = 1)
+	private String sesso;
+
+	@Column(name = "data_nascita", nullable = false)
+	private String dataNascita;
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMatricola() {
+		return matricola;
+	}
+
+	public void setMatricola(String matricola) {
+		this.matricola = matricola;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getSesso() {
+		return sesso;
+	}
+
+	public void setSesso(String sesso) {
+		this.sesso = sesso;
+	}
+
+	public String getDataNascita() {
+		return dataNascita;
+	}
+
+	public void setDataNascita(String dataNascita) {
+		this.dataNascita = dataNascita;
+	}
+
+	@Override
+	public String toString() {
+		return "Studente [id=" + id + ", matricola=" + matricola + ", nome=" + nome + ", cognome=" + cognome
+				+ ", sesso=" + sesso + ", dataNascita=" + dataNascita + "]";
+	}
+	
+	
 }
