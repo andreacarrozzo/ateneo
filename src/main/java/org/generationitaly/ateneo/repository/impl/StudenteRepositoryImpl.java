cc04922f00398840fd521883e2e47bb9be882fbb
andre <andre@AndreaPC.station>
2024-04-15 15:21:03 +0200

    studenterep

---
+++ src/main/java/org/generationitaly/ateneo/repository/impl/StudenteRepositoryImpl.java
@@ -0,0 +1,8 @@
+package org.generationitaly.ateneo.repository.impl;
+
+import org.generationitaly.ateneo.entity.Studente;
+import org.generationitaly.ateneo.repository.StudenteRepository;
+
+public class StudenteRepositoryImpl extends JpaRepositoryImpl<Studente, Integer> implements StudenteRepository{
+
+}
