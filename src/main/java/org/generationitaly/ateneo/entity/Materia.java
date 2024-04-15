2ba09fc03c1a5d7ddf0fc8be7eee76e9b8562d63
Ale <Ale@DESKTOP-TSCKJ3E.homenet.telecomitalia.it>
2024-04-15 14:27:37 +0200

    Materia fatta

--- src/main/java/org/generationitaly/ateneo/entity/Materia.java
+++ src/main/java/org/generationitaly/ateneo/entity/Materia.java
@@ -1,5 +1,74 @@
 package org.generationitaly.ateneo.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/*
+ * CREATE TABLE materia (
+	id INT NOT NULL AUTO_INCREMENT,
+    codice CHAR(5) NOT NULL,
+    nome VARCHAR(45) NOT NULL,
+    cfu INT NOT NULL,
+    PRIMARY KEY(id),
+    UNIQUE(codice),
+    CHECK(cfu > 0)
+);
+ */
+
+@Entity
+@Table(name = "materia")
 public class Materia {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	private int id;
+	@Column(name = "codice", length = 5, unique = true, nullable = false)
+	private String codice;
+	@Column(name = "nome", length = 45, nullable = false)
+	private String nome;
+	@Column(name = "cfu", nullable = false)
+	private int cfu;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCodice() {
+		return codice;
+	}
+
+	public void setCodice(String codice) {
+		this.codice = codice;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getCfu() {
+		return cfu;
+	}
+
+	public void setCfu(int cfu) {
+		this.cfu = cfu;
+	}
+
+	@Override
+	public String toString() {
+		return "Materia [id=" + id + ", codice=" + codice + ", nome=" + nome + ", cfu=" + cfu + "]";
+	}
 
 }
