5f50cffe4854a177143b4d79046c0e1280803f70
Ale <Ale@DESKTOP-TSCKJ3E.homenet.telecomitalia.it>
2024-04-15 15:21:29 +0200

    Fatto capo

---
+++ src/main/java/org/generationitaly/ateneo/repository/MateriaRepository.java
@@ -0,0 +1,10 @@
+package org.generationitaly.ateneo.repository;
+
+
+
+import org.generationitaly.ateneo.entity.Materia;
+
+public interface MateriaRepository extends JpaRepository<Materia,Integer> {
+	
+
+}
---
+++ src/main/java/org/generationitaly/ateneo/repository/impl/MateriaRepositoryImpl.java
@@ -0,0 +1,9 @@
+package org.generationitaly.ateneo.repository.impl;
+
+import org.generationitaly.ateneo.entity.Materia;
+import org.generationitaly.ateneo.repository.MateriaRepository;
+
+public class MateriaRepositoryImpl extends JpaRepositoryImpl<Materia, Integer>  implements MateriaRepository{
+
+
+}
